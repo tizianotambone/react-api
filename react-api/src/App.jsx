93020ed5ed52c8f063060a2d7aef6e8c159ab70d
tizianotambone <tambonetiziano@gmail.com>
2025-05-13 14:56:45 +0200

    [UPD]chiamata ajax

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  
+  const [actress, setActress] = useState([]);
+  const fetchActress = () => {
+    axios
+      .get('https://lanciweb.github.io/demo/api/actresses/')
+      .then((response) => {
+        setActress(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error('Errore nella chiamata API:', error);
+      });
+      
+    };
+    useEffect(() => {
+      fetchActress();
+    }, []);
+
 
   return (
     <>
