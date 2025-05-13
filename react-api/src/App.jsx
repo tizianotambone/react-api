e0cc65aafb2117df61289ee1f0ca213c0f78d842
tizianotambone <tambonetiziano@gmail.com>
2025-05-13 15:42:31 +0200

    [UPD]stilizzazione cards

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -26,22 +26,29 @@ function App() {
       <div className="container">
         <div className='row gy-4'>
           <div className='col-12'>
-          <h1>Actress List</h1>
+            <h1 className='text-center mt-3'>ACTRESSES LIST</h1>
           </div>
         </div>
       </div>
-       <div className='row gy-3'>
-        {/* ciclo actress tramite map */}
+       
+      <div className='row gy-3'>
         {actress.map((actress) => (
-          <div key={`actress-${actress.id}`} className='col-12 col-md-6 col-lg-4'>
+          <div key={`actress-${actress.id}`} className='col-12 col-md-6 col-lg-3'>
             <div className='card rounded-3'>
               <div className='actress-img'>
                 <img src={actress.image} alt={actress.name} className='img-fluid' />
               </div>
+              <div className='actress-info p-3'>
+                <h3 className='actress-name'>{actress.name}</h3>
+                <p className='actress-birth'>{actress.birth_year}</p>
+                <p className='actress-nationality'>{actress.nationality}</p>
+                <p className='actress-awards'>{actress.awards}</p>
+                <p className='actress-biography'>{actress.biography}</p>
+                
+              </div>
             </div>
           </div>
         ))}
-
       </div>
       
 
